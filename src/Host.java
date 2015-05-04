140742f1d39232fbc02a8f6f5451d1479dc24e39
Rahul <rahul.1186@gmail.com>
2015-05-04 17:46:22 -0500

    Updated server code
    Server code updated and attributes added to Host and Message

--- src/Host.java
+++ src/Host.java
@@ -14,28 +14,67 @@ public class Host implements Serializable{
 	public int hostId;
 	public String hostName;
 	public int hostPort;
-	public boolean keyKnown;
-	public boolean isRequested;
+	public int versionNumber;
+	public int replicasUpdated;
+	public int fileNumber;
+	public String fileName;
+	public byte[] latestContents;
 	public boolean isTerminated;
-	
-	public Host(int hostId, String hostName, int hostPort, boolean keyKnown, boolean isRequested)
-	{
+	public int getHostId() {
+		return hostId;
+	}
+	public void setHostId(int hostId) {
 		this.hostId = hostId;
+	}
+	public String getHostName() {
+		return hostName;
+	}
+	public void setHostName(String hostName) {
 		this.hostName = hostName;
+	}
+	public int getHostPort() {
+		return hostPort;
+	}
+	public void setHostPort(int hostPort) {
 		this.hostPort = hostPort;
-		this.keyKnown = keyKnown;
-		this.isRequested = isRequested;
-		this.isTerminated = false;
+	}
+	public int getVersionNumber() {
+		return versionNumber;
+	}
+	public void setVersionNumber(int versionNumber) {
+		this.versionNumber = versionNumber;
+	}
+	public int getReplicasUpdated() {
+		return replicasUpdated;
+	}
+	public void setReplicasUpdated(int replicasUpdated) {
+		this.replicasUpdated = replicasUpdated;
+	}
+	public int getFileNumber() {
+		return fileNumber;
+	}
+	public void setFileNumber(int fileNumber) {
+		this.fileNumber = fileNumber;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public byte[] getLatestContents() {
+		return latestContents;
+	}
+	public void setLatestContents(byte[] latestContents) {
+		this.latestContents = latestContents;
+	}
+	public boolean isTerminated() {
+		return isTerminated;
+	}
+	public void setTerminated(boolean isTerminated) {
+		this.isTerminated = isTerminated;
 	}
 	
-	public Host()
-	{
-		this.hostId = 0;
-		this.hostName = "";
-		this.hostPort = 0;	
-		this.keyKnown = false;
-		this.isRequested = false;
-		this.isTerminated = false;
-	}
+	
 }
 
--- src/Message.java
+++ src/Message.java
@@ -11,16 +11,57 @@ import java.util.concurrent.atomic.AtomicInteger;
  *
  */
 enum MessageType {
-	REQUEST_KEY,RESPONSE_KEY,RESPONSE_AND_REQUEST_KEY,TERMINATION_MESSAGE;
+	REQUEST_READ_LOCK,REQUEST_WRITE_LOCK,RESPONSE_READ, RESPONSE_WRITE, RELEASE_READ_LOCK, RELEASE_WRITE_LOCK, REQUEST_LATEST_FILE_READ, REQUEST_LATEST_FILE_WRITE;
+}
+
+enum Status {
+	GRANT, DENY;
 }
 public class Message implements Serializable{
-	AtomicInteger timeStamp;
+	
+	int logicalTimeStamp;
+	int[] vectorTimestamp;
 	MessageType messageType;
-	Host nodeInfo;
-	public Message(AtomicInteger timeStamp, MessageType messageType, Host nodeInfo)
-	{
-		this.timeStamp = timeStamp;
-		this.messageType = messageType;
-		this.nodeInfo = nodeInfo;
+	Host sourceNode;
+	Host clientNode;
+	Status status;
+	
+	public int getLogicalTimeStamp() {
+		return logicalTimeStamp;
 	}
+	public void setLogicalTimeStamp(int logicalTimeStamp) {
+		this.logicalTimeStamp = logicalTimeStamp;
+	}
+	public int[] getVectorTimestamp() {
+		return vectorTimestamp;
+	}
+	public void setVectorTimestamp(int[] vectorTimestamp) {
+		this.vectorTimestamp = vectorTimestamp;
+	}
+	public MessageType getMessageType() {
+		return messageType;
+	}
+	public void setMessageType(MessageType messageType) {
+		this.messageType = messageType;
+	}
+	public Host getSourceNode() {
+		return sourceNode;
+	}
+	public void setSourceNode(Host sourceNode) {
+		this.sourceNode = sourceNode;
+	}
+	public Host getClientNode() {
+		return clientNode;
+	}
+	public void setClientNode(Host clientNode) {
+		this.clientNode = clientNode;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	
+	
 }
